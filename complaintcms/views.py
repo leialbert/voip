389223db6f946de7a2f244a632ac9444c46a6dde
Albert Lei <albert.lei@hotmail.com>
2021-12-30 00:53:02 +0300

    增加Image和File上传功能

--- complaintcms/views.py
+++ complaintcms/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from .forms import UploadFileForm
+from .models import Complaint
 
 # Create your views here.
 def index(request):
     return render(request, 'complaintcms/index.html')
 
 def result(request):
-    return render(request, 'complaintcms/result.html')
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        file = request.FILES.getlist('file')
+        complaint = Complaint.objects.create(cszl_mp3 = file)
+        complaint.save()
+    else:
+        form = UploadFileForm()
+    return render(request, 'complaintcms/result.html',{'form':form})
