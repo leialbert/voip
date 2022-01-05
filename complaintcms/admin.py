34575ce457271ed6917fc3e4af624af74a3b6fb5
Albert Lei <albert.lei@hotmail.com>
2022-01-05 04:08:58 +0300

    显示所有字段

--- complaintcms/admin.py
+++ complaintcms/admin.py
@@ -4,5 +4,5 @@ from .models import Complaint
 
 @admin.register(Complaint)
 class ComplaintAdmin(admin.ModelAdmin):
-    fields = ('tousuid','jbhm','bjbhm','bllx','ld_date','thsc','jbnr','who','csjg','bcsr','yqje','yqts','is_br','csms','is_cszl','is_tjyd','cszl_img1','cszl_img2','cszl_img3','cszl_mp3')
+    # fields = ('tousuid','jbhm','bjbhm','bllx','ld_date','thsc','jbnr','who','csjg','bcsr','yqje','yqts','is_br','csms','is_cszl','is_tjyd','cszl_img1','cszl_img2','cszl_img3','cszl_mp3')
     list_display = ('tousuid','jbhm','bllx','ld_date','thsc','who','is_cszl','is_tjyd')
