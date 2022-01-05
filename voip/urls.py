d9716e47e61014120edcd45daca91cee1d8112b8
Albert Lei <albert.lei@hotmail.com>
2022-01-05 04:21:15 +0300

    修改了下首页路由

--- voip/urls.py
+++ voip/urls.py
@@ -3,8 +3,9 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from complaintcms.views import index
 urlpatterns = [
+    path('',index),
     path('complaint/', include('complaintcms.urls')),
     path('admin/', admin.site.urls),
 ]
