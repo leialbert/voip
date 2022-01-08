26d501fbbff76b9568c41575b7a156c3136bae0e
Albert Lei <albert.lei@hotmail.com>
2022-01-09 00:26:06 +0300

    部署

--- voip/settings.py
+++ voip/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=c92p^%&hs(_%s@j05!bw=l@&uh+pr&%c2yckpjo!qfj!9b%%#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 
 # Application definition
