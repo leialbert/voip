b0086b16169e0ae9f17b2b76d9ac6ba0bbf189ff
Albert Lei <albert.lei@hotmail.com>
2022-01-08 22:32:01 +0300

    增加项目模板文件夹

--- complaintcms/urls.py
+++ complaintcms/urls.py
@@ -4,7 +4,4 @@ from . import views
 urlpatterns = [ 
     path('', views.index, name='index'),
     path('result/',views.result,name='result'),
-    path('parse/',views.parse, name='parse'),
-    path('saveto/',views.saveto, name='saveto'),
-    # path('upload/',views.upload,name='upload'),
 ]
